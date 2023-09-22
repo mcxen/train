927ec9d113400f5b75bde32698607b7e911fc6cb
MCX-LAB <imsalt@icloud.com>
2023-09-22 21:23:23 +0800

    新增公共模块，新增公共的配置文件

--- member/src/main/java/com/mcxgroup/member/config/MemberApplication.java
+++ member/src/main/java/com/mcxgroup/member/config/MemberApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.mcxgroup")
@@ -13,8 +14,11 @@ public class MemberApplication {
 
     //主项目
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+
+//        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
         log.info("member模块启动成功");
-        log.info("测试地址: \t http://localhost:8080/member/1");
+        log.info("测试地址: \thttp://127.0.0.1:{}{}/1", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 }
