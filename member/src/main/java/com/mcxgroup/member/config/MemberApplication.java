2543ae307f6d63c1e91c30d85602b95d5530901b
MCX-LAB <imsalt@icloud.com>
2023-09-22 20:06:51 +0800

    新增log模块

--- member/src/main/java/com/mcxgroup/member/config/MemberApplication.java
+++ member/src/main/java/com/mcxgroup/member/config/MemberApplication.java
@@ -1,5 +1,6 @@
 package com.mcxgroup.member.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,11 +8,12 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.mcxgroup")
 //Scan不加的扫描不到隔壁目录的TestController
-
+@Slf4j
 public class MemberApplication {
 
     //主项目
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class, args);
+        log.info("member模块启动成功");
     }
 }
