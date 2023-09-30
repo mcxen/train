abde12aafb83b2d956d8fca003b46913a6da655d
mcxw <imsalt@icloud.com>
2023-09-30 09:56:50 +0800

    使用Vue(Store)来保存信息

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,12 @@
 <template>
   <a-layout-header class="header">
     <div class="logo" />
+    <div style="float: right; color: white;">
+      您好：{{member.mobile}} &nbsp;&nbsp;
+      <router-link to="/login" style="color: white;">
+        退出登录
+      </router-link>
+    </div>
     <a-menu
         v-model:selectedKeys="selectedKeys1"
         theme="dark"
@@ -11,6 +17,7 @@
       <a-menu-item key="2">nav 2</a-menu-item>
       <a-menu-item key="3">nav 3</a-menu-item>
     </a-menu>
+
   </a-layout-header>
 </template>
 
@@ -22,9 +29,10 @@ import router from '@/router'
 export default defineComponent({
   name: "the-header-view",
   setup() {
-
+    let member = store.state.member;
     return {
         selectedKeys1: ref(['1']),
+      member
     };
   },
 });
