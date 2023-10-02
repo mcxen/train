26b6f7e845766406518eba4469454f449518b8a6
mcxw <imsalt@icloud.com>
2023-10-02 22:59:43 +0800

    增加日志拦截器，统一打印日志流水号时间戳

--- member/src/main/java/com/mcxgroup/member/config/SpringMvcConfig.java
+++ member/src/main/java/com/mcxgroup/member/config/SpringMvcConfig.java
@@ -1,6 +1,7 @@
 package com.mcxgroup.member.config;
 
 
+import com.mcxgroup.common.interceptor.LogInterceptor;
 import com.mcxgroup.common.interceptor.MemberInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
@@ -10,15 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-//   @Resource
-//   LogInterceptor logInterceptor;
+   @Resource
+   LogInterceptor logInterceptor;
 
    @Resource
    MemberInterceptor memberInterceptor;
 
    @Override
    public void addInterceptors(InterceptorRegistry registry) {
-//       registry.addInterceptor(logInterceptor);
+       //开启Log登录之后的拦截器，解决打印的日志没有时间戳的问题
+       registry.addInterceptor(logInterceptor);
 
        // 路径不要包含context-path
        registry.addInterceptor(memberInterceptor)
