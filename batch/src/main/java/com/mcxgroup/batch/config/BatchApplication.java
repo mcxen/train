7f5fb2ab7228996fd93e26c58216ffbb76e8d50d
mcxw <imsalt@icloud.com>
2023-10-24 16:01:21 +0800

    新增批处理Batch模块

---
+++ batch/src/main/java/com/mcxgroup/batch/config/BatchApplication.java
@@ -0,0 +1,27 @@
+package com.mcxgroup.batch.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.mcxgroup")
+//Scan不加的扫描不到隔壁目录的TestController
+@MapperScan("com.mcxgroup.*.mapper")
+//@Configuration
+@Slf4j
+public class BatchApplication {
+
+    //主项目
+    public static void main(String[] args) {
+
+//        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("Batch Application模块启动成功");
+        log.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
+}
