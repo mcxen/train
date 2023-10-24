4c1ae77e90505a227e32ea08d673cae2454610be
mcxw <imsalt@icloud.com>
2023-10-24 21:30:16 +0800

    增加批处理的页面

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -9,23 +9,32 @@ const routes = [
             path: 'welcome',
             component: () => import('../views/main/welcome.vue')
         }, {
-            path: 'station',
-            component: () => import('../views/main/station.vue')
-        }, {
-            path: 'train',
-            component: () => import('../views/main/train.vue')
-        },{
-            path: 'train-station',
-            component: () => import('../views/main/train-station.vue')
-        },{
-            path: 'train-carriage',
-            component: () => import('../views/main/train-carriage.vue')
-        },{
-            path: 'train-seat',
-            component: () => import('../views/main/train-seat.vue')
-        },{
             path: 'about',
             component: () => import('../views/main/about.vue')
+        }, {
+            path: 'base/',
+            children: [{
+                path: 'station',
+                component: () => import('../views/main/station.vue'),
+            }, {
+                path: 'train',
+                component: () => import('../views/main/train.vue'),
+            }, {
+                path: 'train-station',
+                component: () => import('../views/main/train-station.vue'),
+            }, {
+                path: 'train-carriage',
+                component: () => import('../views/main/train-carriage.vue'),
+            }, {
+                path: 'train-seat',
+                component: () => import('../views/main/train-seat.vue'),
+            }]
+        }, {
+            path: 'batch/',
+            children: [{
+                path: 'job',
+                component: () => import('../views/main/job.vue')
+            }]
         }]
     }, {
         path: '',
