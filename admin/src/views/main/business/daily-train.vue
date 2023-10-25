24c8c2b6a2b1cef50b8621cbd964da10060e8da5
mcxw <imsalt@icloud.com>
2023-10-25 21:49:54 +0800

    为每日车次表单增加车次下拉选择，并自动填充车次相关数据

--- admin/src/views/main/business/daily-train.vue
+++ admin/src/views/main/business/daily-train.vue
@@ -2,7 +2,7 @@
   <p>
     <a-space>
       <a-button type="primary" @click="handleQuery()">刷新</a-button>
-      
+      <a-button type="primary" @click="onAdd">新增</a-button>
     </a-space>
   </p>
   <a-table :dataSource="dailyTrains"
@@ -12,6 +12,16 @@
            :loading="loading">
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
+        <a-space>
+          <!--删除按钮，增加确认的框-->
+          <a-popconfirm
+              title="删除后不可恢复，确认删除?"
+              @confirm="onDelete(record)"
+              ok-text="确认" cancel-text="取消">
+            <a style="color: red">删除</a>
+          </a-popconfirm>
+          <a @click="onEdit(record)">编辑</a>
+        </a-space>
       </template>
       <template v-else-if="column.dataIndex === 'type'">
         <span v-for="item in TRAIN_TYPE_ARRAY" :key="item.code">
@@ -22,15 +32,53 @@
       </template>
     </template>
   </a-table>
+  <a-modal v-model:visible="visible" title="每日车次" @ok="handleOk"
+           ok-text="确认" cancel-text="取消">
+    <a-form :model="dailyTrain" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+      <a-form-item label="日期">
+        <a-date-picker v-model:value="dailyTrain.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
+      </a-form-item>
+      <a-form-item label="车次编号">
+        <train-select-view v-model:value="dailyTrain.code" @change="onChangeCode"/>
+      </a-form-item>
+      <a-form-item label="车次类型">
+        <a-select v-model:value="dailyTrain.type">
+          <a-select-option v-for="item in TRAIN_TYPE_ARRAY" :key="item.code" :value="item.code">
+            {{item.desc}}
+          </a-select-option>
+        </a-select>
+      </a-form-item>
+      <a-form-item label="始发站">
+        <a-input v-model:value="dailyTrain.start" />
+      </a-form-item>
+      <a-form-item label="始发站拼音">
+        <a-input v-model:value="dailyTrain.startPinyin" />
+      </a-form-item>
+      <a-form-item label="出发时间">
+        <a-time-picker v-model:value="dailyTrain.startTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
+      </a-form-item>
+      <a-form-item label="终点站">
+        <a-input v-model:value="dailyTrain.end" />
+      </a-form-item>
+      <a-form-item label="终点站拼音">
+        <a-input v-model:value="dailyTrain.endPinyin" />
+      </a-form-item>
+      <a-form-item label="到站时间">
+        <a-time-picker v-model:value="dailyTrain.endTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
 
 <script>
 import { defineComponent, ref, onMounted } from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
+import TrainSelectView from "@/components/train-select.vue";
 
 export default defineComponent({
   name: "daily-train-view",
+  components: {TrainSelectView},
   setup() {
     // 全局的window，直接引用import './assets/js/enums';//引入enums.js，
     const TRAIN_TYPE_ARRAY = window.TRAIN_TYPE_ARRAY;
@@ -103,8 +151,53 @@ export default defineComponent({
       dataIndex: 'endTime',
       key: 'endTime',
     },
+    {
+      title: '操作',
+      dataIndex: 'operation'
+    }
     ];
 
+    const onAdd = () => {
+      dailyTrain.value = {};
+      visible.value = true;
+    };
+
+    const onEdit = (record) => {
+      dailyTrain.value = window.Tool.copy(record);
+      visible.value = true;
+    };
+
+    const onDelete = (record) => {
+      axios.delete("/business/admin/daily-train/delete/" + record.id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          notification.success({description: "删除成功！"});
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize,
+          });
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    const handleOk = () => {
+      axios.post("/business/admin/daily-train/save", dailyTrain.value).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({description: "保存成功！"});
+          // 点击ok的时候就会把模态框关闭
+          visible.value = false;
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize
+          });
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
 //如果没有初值的话就增加一个初始的参数首页为1页
     const handleQuery = (param) => {
       if (!param) {
@@ -142,6 +235,14 @@ export default defineComponent({
       });
     };
 
+    const onChangeCode = (train) => {
+      console.log("车次下拉组件选择：", train);
+      let t = Tool.copy(train);
+      delete t.id;
+      // 用assign可以合并
+      dailyTrain.value = Object.assign(dailyTrain.value, t);
+    };
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -159,6 +260,11 @@ export default defineComponent({
       handleTableChange,
       handleQuery,
       loading,
+      onAdd,
+      handleOk,
+      onEdit,
+      onDelete,
+      onChangeCode
     };
   },
 });
