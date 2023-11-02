b2dc51e4953dd8525e1a74f0173160ef0c53b0b7
mcxw <imsalt@icloud.com>
2023-11-02 19:14:20 +0800

    完成定时任务生成每日火车数据-@Pathval("date")

--- batch/src/main/java/com/mcxgroup/batch/feign/BusinessFeign.java
+++ batch/src/main/java/com/mcxgroup/batch/feign/BusinessFeign.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Date;
-@FeignClient(name = "business", url = "http://127.0.0.1:8002/business")
+@FeignClient(name = "business", url = "http://127.0.0.1:8081/business")
 public interface BusinessFeign {
 
     @GetMapping("/hello")
     String hello();
 
     @GetMapping("/admin/daily-train/gen-daily/{date}")
-    CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
+    CommonResp<Object> genDaily(@PathVariable("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 }
