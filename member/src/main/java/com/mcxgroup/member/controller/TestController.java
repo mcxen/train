201d8852d76977cb44748feba360d7209dcaeb33
mcxw <imsalt@icloud.com>
2023-11-14 21:39:27 +0800

    添加配置中心Nacos，统一管理部分配置，添加RefreshScope实时刷新

--- member/src/main/java/com/mcxgroup/member/controller/TestController.java
+++ member/src/main/java/com/mcxgroup/member/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.mcxgroup.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//Cloud的原始的规范，自动刷新配置中心的数据，与nacos无关
+@RefreshScope
 public class TestController {
+    @Value("${test.nacos}")
+    private String nacosTest;
 
     @GetMapping("/1")
     public String get(){
-        return "hello";
+        return "hello"+nacosTest;
     }
 
 
