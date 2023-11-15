0c8ff79ca06e027d6caebaecabea847e48987ca0
mcxw <imsalt@icloud.com>
2023-11-15 21:26:33 +0800

    配置注册中心和nacos

--- batch/src/main/java/com/mcxgroup/batch/controller/TestController.java
+++ batch/src/main/java/com/mcxgroup/batch/controller/TestController.java
@@ -1,23 +1,23 @@
-//package com.mcxgroup.batch.controller;
-//
-//import com.mcxgroup.batch.feign.BusinessFeign;
-//import jakarta.annotation.Resource;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@Slf4j
-//public class TestController {
-//
-//    @Resource
-//    BusinessFeign businessFeign;
-//    @GetMapping("/hello")
-//    public String get(){
-////        String s = businessFeign.hello1();
-////        System.out.println("s = " + s);
-////        log.info(s);
-//        return "Hello Batch";
-//    }
-//
-//}
+package com.mcxgroup.batch.controller;
+
+import com.mcxgroup.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @Resource
+    BusinessFeign businessFeign;
+    @GetMapping("/hello")
+    public String get(){
+        String s = businessFeign.hello();
+        System.out.println("s = " + s);
+        log.info(s);
+        return "Hello Batch"+s;
+    }
+
+}
--- batch/src/main/java/com/mcxgroup/batch/feign/BusinessFeign.java
+++ batch/src/main/java/com/mcxgroup/batch/feign/BusinessFeign.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Date;
-@FeignClient(name = "business", url = "http://127.0.0.1:8081/business")
+//@FeignClient(name = "business", url = "http://127.0.0.1:8081/business")
+@FeignClient(name = "business")
 public interface BusinessFeign {
 
-    @GetMapping("/hello")
+    @GetMapping("/business/hello")
     String hello();
 
-    @GetMapping("/admin/daily-train/gen-daily/{date}")
+    @GetMapping("/business/admin/daily-train/gen-daily/{date}")
     CommonResp<Object> genDaily(@PathVariable("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 }
